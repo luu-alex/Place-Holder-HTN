71a43c97a1c58f7ca14100ca3ed3ff7dbd324aa9
Nancy Wang <nancywang882@gmail.com>
2019-09-14 22:51:13 -0400

    updtaed navigation

--- my-app/src/components/Navigation.js
+++ my-app/src/components/Navigation.js
@@ -1,28 +1,59 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 class Navigation extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      tabKey: "/home"
+    };
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(tabKey) {
+      this.setState({ tabKey : tabKey });
+  }
+
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">PlaceHolder</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <div>
+        <Navbar bg="light" variant="light">
+          <Navbar.Brand href="#home">Placeholder</Navbar.Brand>
+          <Nav
+            activeKey="/home"
+            onSelect={selectedKey => this.handleClick(selectedKey)}
+          >
+            <Nav.Item>
+              <Nav.Link href="/home">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-1">Library</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-2">Train</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">About <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item active">
-              <a className="nav-link" href="/">Library <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item active">
-              <a className="nav-link" href="/">Train <span className="sr-only">(current)</span></a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+        {
+           this.state.tabKey == "/home" &&
+           <h2>NONCY</h2>
+        }
+
+        {
+           this.state.tabKey == "link-1" &&
+           <h3>hello</h3>
+        }
+
+        {
+          this.state.tabKey == "link-2" &&
+          <h3>Hello</h3>
+        }
+      </div>
     )
   }
 }
